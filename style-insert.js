1eb9d0a76e4e0f2e9e041c3826456e3b5d1e244d
Pavel Birukov <birukov.pavel@gmail.com>
2014-02-10 23:24:16 +0300

    Styles

--- style-insert.js
+++ style-insert.js
@@ -1,3 +1,12 @@
-window.jQuery(function(){
-    $('head').append($('<style type="text/css" href="http://r00ger.com" />'));
+$(function () {
+    $('head').append($('<link rel="stylesheet" type="text/css" href="http://r00ger.com">'));
 })
+
+
+//document.getElementById('lpc').style.setProperty('display', 'none')
+//document.onload = function () {
+//    var bl = document.querySelectorAll('a[target="_blank"]');
+//    for (var i in bl) {
+//        bl[i].setAttritute('class', 'display:none;')
+//    }
+//}
