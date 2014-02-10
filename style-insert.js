39dce569b22c6139330c92df38cd2f95c05abbd4
Pavel Birukov <birukov.pavel@gmail.com>
2014-02-11 00:06:02 +0300

    logo, inner styles

--- style-insert.js
+++ style-insert.js
@@ -1,6 +1,6 @@
 var appendStyles = function(){
-//    $('head').append($('<link rel="stylesheet" type="text/css" />').attr('href', chrome.extension.getURL("hide-ad.css")));
-    $('head').append($('<link rel="stylesheet" type="text/css" href="https://raw2.github.com/r00ger/seasonvar-no-ad/master/hide-ad.css"/>'));
+    $('head').append($('<link rel="stylesheet" type="text/css" />').attr('href', chrome.extension.getURL("hide-ad.css")));
+//    $('head').append($('<link rel="stylesheet" type="text/css" href="https://raw2.github.com/r00ger/seasonvar-no-ad/master/hide-ad.css"/>'));
 }
 
 if ($('head').length) {
