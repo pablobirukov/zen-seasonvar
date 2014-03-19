dc440aa496c0e2a4f4d4cc8581122effa4020675
Pavel Birukov <birukov.pavel@gmail.com>
2014-03-19 14:39:01 +0300

    remove flash ad

--- style-insert.js
+++ style-insert.js
@@ -1,6 +1,6 @@
 var appendStyles = function(){
     $('head').append($('<link rel="stylesheet" type="text/css" />').attr('href', chrome.extension.getURL("hide-ad.css")));
-//    $('head').append($('<link rel="stylesheet" type="text/css" href="https://raw2.github.com/r00ger/seasonvar-no-ad/master/hide-ad.css"/>'));
+$('object').each(function(i, o){ if ($(o).css('position') === 'fixed') $(o).hide();})
 }
 
 if ($('head').length) {
