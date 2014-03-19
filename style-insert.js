f410ba2bff98d22733c175d72ee97e791c0fc70c
Pavel Birukov <birukov.pavel@gmail.com>
2014-03-19 14:39:26 +0300

    remove flash ad

--- style-insert.js
+++ style-insert.js
@@ -1,6 +1,6 @@
 var appendStyles = function(){
     $('head').append($('<link rel="stylesheet" type="text/css" />').attr('href', chrome.extension.getURL("hide-ad.css")));
-$('object').each(function(i, o){ if ($(o).css('position') === 'fixed') $(o).hide();})
+	$('object').each(function(i, o){ if ($(o).css('position') === 'fixed') $(o).hide();})
 }
 
 if ($('head').length) {
