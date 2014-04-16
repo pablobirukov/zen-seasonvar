527abcb5bd049bbe1829b5345799e3fbca1c33ac
Pavel Birukov <birukov.pavel@gmail.com>
2014-04-17 01:11:22 +0300

    new player layout

--- style-insert.js
+++ style-insert.js
@@ -1,10 +1,13 @@
-var appendStyles = function(){
-    $('head').append($('<link rel="stylesheet" type="text/css" />').attr('href', chrome.extension.getURL("hide-ad.css")));
-	$('object').each(function(i, o){ if ($(o).css('position') === 'fixed') $(o).hide();})
-}
 
-if ($('head').length) {
-    appendStyles();
-} else {
-    $(appendStyles);
-}
+(function($){
+    $(function(){
+        if (location.href.indexOf('serial') === -1) return;
+        var $clearFrame = $('<div id="clear-frame" style="height: 100%;"></div>'),
+            $body = $('body'),
+            $video = $('object'),
+            $menu = $('.td-for-content h2>a').parent().css({'text-align': 'left', margin: '10px'});
+        $body.append($clearFrame.append($('#logo')).append($menu).append($video)).attr('style', 'padding: 0px !important; height: 100% !important;');
+        $('body > *').not($clearFrame).hide();
+        $('html').css('height', '100%');
+    })
+}(jQuery))
