7523013702124c37499f9686f8e8c540fb0c3427
Pavel Birukov <birukov.pavel@gmail.com>
2014-12-20 14:33:20 +0300

    bookmark buttons

--- style-insert.js
+++ style-insert.js
@@ -5,8 +5,10 @@
         var $clearFrame = $('<div id="clear-frame" style="height: 100%;"></div>'),
             $body = $('body'),
             $video = $('object'),
-            $menu = $('.td-for-content h2>a').parent().css({'text-align': 'left', margin: '10px'});
-        $body.append($clearFrame.append($('#logo')).append($menu).append($video)).attr('style', 'padding: 0px !important; height: 100% !important;');
+            $menu = $('.td-for-content h2>a').parent().css({'text-align': 'left', margin: '10px'}),
+            $marks = $('#addMarks'),
+            $otm = $('div.otm');
+        $body.append($clearFrame.append($('#logo')).append($menu).append($otm).append($marks).append($video)).attr('style', 'padding: 0px !important; height: 100% !important;');
         $('body > *').not($clearFrame).hide();
         $('html').css('height', '100%');
     })
